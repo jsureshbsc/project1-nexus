c04099ae9de814176aabe115c9bb5feccc399797
jsureshbsc <142655593+jsureshbsc@users.noreply.github.com>
2024-01-09 17:13:25 +0530

    Add files via upload

--- Task-1/script.js
+++ Task-1/script.js
@@ -2,32 +2,43 @@ function validateLoginForm(event) {
     
     const username = document.querySelector('.front input[type="text"]').value;
     const password = document.querySelector('.front input[type="password"]').value;
+
+    
+
     // Check if username and password 
     if (username.trim() === '' && password.trim() === '') {
         alert('Login Successfully...!');
+        window.location.href = "";
         return;
     } else {
         alert('Invalid username or password');
     }
-    event.preventDefault(); // Prevent form submission by default
+    
+    // Prevent form submission by default
+    event.preventDefault(); 
 }
+
+
 function validateSignupForm(event) {
-    event.preventDefault(); // Prevent form submission by default
+   
     const name = document.querySelector('.back input[placeholder="Name"]').value;
     const email = document.querySelector('.back input[placeholder="Email id"]').value;
     const username = document.querySelector('.back input[placeholder="Username"]').value;
     const password = document.querySelector('.back input[placeholder="Password"]').value;
+
+    
+      
     //Check if all fields are not empty
-    // If validation passes, you can submit the form or perform additional actions
     if (name.trim() === '' || email.trim() === '' || username.trim() === '' || password.trim() === '') {
-        alert('Succsufully...!');
+        alert('All fields are required');
         return;
     }
-    else if((name.trim() === '' || email.trim() === '' || username.trim() === '' || password.trim() === '')){
-        alert('All fields are required')
+    else if((name.trim() === 'suresh' || email.trim() === '123@gmail.com' || username.trim() === 'suresh' || password.trim() === '1234')){
+        alert("Succsufully...!")
+        location.href = "";
     }
-    else{
-        alert('Something went wrong')
+     // Prevent form submission by default
+     event.preventDefault(); 
 }
 
 let front = document.querySelector('.front');
